7bad7c7a6de9eb3ca40e1e40227aeb5c44cb34dc
Orbedi <orlandobedi@outlook.es>
2019-05-09 10:30:27 +0100

    Implementación seguridad en add-transfers-parts

--- src/providers/add-transfers-parts/add-transfers-parts.ts
+++ src/providers/add-transfers-parts/add-transfers-parts.ts
@@ -1,7 +1,7 @@
 import {HttpClient, HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {ServerConfig} from "../serverConfig";
-import {Observable} from "rxjs";
+import {ServerConfig} from '../serverConfig';
+import { Storage } from "@ionic/storage";
 
 @Injectable()
 export class AddTransfersPartsProvider {
@@ -9,15 +9,26 @@ export class AddTransfersPartsProvider {
   private serverConf = new ServerConfig();
   private url:string = this.serverConf.getRootDir().concat("services");
   private readonly httpOptions;
+  private username: string;
+  private token: string;
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient, public storage: Storage) {
     console.log('Hello AddTransfersPartsProvider Provider');
   }
 
   getByDate(transferDate){
-      let param = new HttpParams().append('transferDate', transferDate);
-      return this.http.post<any>(this.url.concat("/getByDate.php"), param, this.httpOptions);
+    let param;
+    this.getCredentials();
+    param = new HttpParams().append('transferDate', transferDate)
+                            .append('username', this.username)
+                            .append('token', this.token);
+    return this.http.post<any>(this.url.concat("/getByDate.php"), param, this.httpOptions);
   }
 
-
+  private getCredentials() {
+    this.storage.get("session").then((data) => {
+      this.username = data.username;
+      this.token = data.token;
+    });
+  }
 }
